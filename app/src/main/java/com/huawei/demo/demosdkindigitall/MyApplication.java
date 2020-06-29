095be91118df5ae3b9ce7a2484a8cc3c7a82677a
franalma <franalmeida@gmail.com>
2020-06-29 09:56:32 +0200

    Update MyApplication.java

--- app/src/main/java/com/huawei/demo/demosdkindigitall/MyApplication.java
+++ app/src/main/java/com/huawei/demo/demosdkindigitall/MyApplication.java
@@ -23,26 +23,26 @@ public class MyApplication extends Application {
 
     private void initSdk(){
 
-//        Indigitall.init(this,INDIGITALL_PRIVATE_KEY, SENDER_ID, new InitCallBack(this){
-//            @Override
-//            public void onErrorInitialized(String s) {
-//                super.onErrorInitialized(s);
-//                System.out.println("----error: "+s);
-//            }
-//
-//            @Override
-//            public void onNewUserRegistered(Device device) {
-//                super.onNewUserRegistered(device);
-//                System.out.println("---new user: "+device.getDeviceId());
-//            }
-//
-//            @Override
-//            public void onIndigitallInitialized(Permission[] permissions, Device device) {
-//                super.onIndigitallInitialized(permissions, device);
-//                System.out.println("--on init: "+device.getDeviceId());;
-//            }
-//        });
-        Indigitall.init(this,INDIGITALL_PRIVATE_KEY, SENDER_ID);
-        System.out.println("---after init");
+        Indigitall.init(this,INDIGITALL_PRIVATE_KEY, SENDER_ID, new InitCallBack(this){
+            @Override
+            public void onErrorInitialized(String s) {
+                super.onErrorInitialized(s);
+                System.out.println("----error: "+s);
+            }
+
+            @Override
+            public void onNewUserRegistered(Device device) {
+                super.onNewUserRegistered(device);
+                System.out.println("---new user: "+device.getDeviceId());
+            }
+
+            @Override
+            public void onIndigitallInitialized(Permission[] permissions, Device device) {
+                super.onIndigitallInitialized(permissions, device);
+                System.out.println("--on init: "+device.getDeviceId());;
+            }
+        });
+//        Indigitall.init(this,INDIGITALL_PRIVATE_KEY, SENDER_ID);
+//        System.out.println("---after init");
     }
 }
